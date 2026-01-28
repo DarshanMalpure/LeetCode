20f272d49bf0c1491aef7cf2e3c153672ddf655e
Darshan Malpure <137028217+DarshanMalpure@users.noreply.github.com>
2026-01-29 01:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (59.42%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int f(int i,int j,vector<vector<int>>&grid,int n,int m,vector<vector<int>>&dp){
         // int total=0;
-        if(i>=n || j>=m) return dp[i][j]=INT_MAX;
+        if(i>=n || j>=m) return INT_MAX;
         if(dp[i][j]!=-1) return dp[i][j];
         if(i==n-1 && j==m-1) return dp[i][j]=grid[i][j];
         int right=f(i,j+1,grid,n,m,dp);
