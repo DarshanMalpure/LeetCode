5406f95d0b832ade9224b3377ac52ab79b287aa6
Darshan Malpure <137028217+DarshanMalpure@users.noreply.github.com>
2026-01-30 00:24:12 +0530

    Time: 11 ms (31.28%) | Memory: 100.9 MB (6.60%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     int minSwaps(vector<int>& nums) {
         vector<int>arr;
+        int totalOnes=0;
         for(int it:nums){
+            if(it==1) totalOnes++;
             arr.push_back(it);
         }
         for(int it:nums){
             arr.push_back(it);
         }
-        int countOnes=0;
-        for(int it:nums){
-            if(it==1) countOnes++;
+        int n=nums.size();
+        int currOnes=0;
+        int maxCount=0;
+        int i=0;
+        int j=0;
+        while(j<2*n){
+            if(arr[j]==1) currOnes++;
+            if(j-i+1>totalOnes){
+                currOnes-=arr[i];
+                i++;
+            }
+            maxCount=max(maxCount,currOnes);
+            j++;
         }
-        int n=arr.size();
-        int maxOnes=0;
-        for(int i=0;i<countOnes;i++){
-            if(arr[i]==1) maxOnes++;
-        }
-        int minSwaps=INT_MAX;
-        int x=maxOnes;
-        for(int i=1;i<=n-countOnes;i++){
-            if(arr[i-1]==1) maxOnes--;
-            if(arr[i+countOnes-1]==1) maxOnes++;
-            if(x<maxOnes) x=maxOnes;
-        }
-        int numZeroes=countOnes-x;
-        return numZeroes;
+        return totalOnes-maxCount;
     }
 };
